fe9a0ff76422f74eded0efbc17b754070d835173
jws <350331045@qq.com>
2021-11-30 08:55:18 +0800

    修改

--- jdCookie.js
+++ jdCookie.js
@@ -1,7 +1,8 @@
 const axios = require('axios').default;
-const config = require('./config');
+// const config = require('./config');
 const getCookie = async () => {
-    let res = await axios.get(process.env.getCookie || config.url);
+    // let res = await axios.get(process.env.getCookie || config.url);
+    let res = await axios.get(process.env.getCookie);
     return res.data.result;
 };
 
