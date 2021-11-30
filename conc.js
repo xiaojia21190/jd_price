5a7ab8b5c14592d3802eb8609bb82116f3592016
jws <350331045@qq.com>
2021-11-30 09:06:38 +0800

    修复

--- conc.js
+++ conc.js
@@ -5,11 +5,12 @@ const concCk = async () => {
     let cks = await getCookie();
     for (let i = 0; i < cks.length; i++) {
         const element = cks[i];
-        var workerProcess = child_process.exec(`node jd_speed_sign.js ${element}`, function (err, stdout, stderr) {
+        consoele.log(element);
+        var workerProcess = child_process.exec(`node jd_speed_sign.js ${element.value}`, function (err, stdout, stderr) {
             if (!err) {
                 //stdout输出结果，stderr输出错误
-                console.log('stdout:', stdout);
-                console.log('stderr:', stderr);
+                console.log('结果:', stdout);
+                console.log('错误:', stderr);
             } else {
                 console.log(err);
             }
