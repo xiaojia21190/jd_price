c63d41cbf62d64472f02b677a585485a108efcdd
jiaws <350331045@qq.com>
2022-01-24 09:33:01 +0800

    ql

--- jdCookie.js
+++ jdCookie.js
@@ -1,8 +1,8 @@
 const axios = require('axios').default;
-const config = require('./config');
+// const config = require('./config');
 const getCookie = async () => {
     let token = await getToken();
-    let client_url = config.client_url || process.env.client_url;
+    let client_url = process.env.client_url;
     let body = await axios.get(`${client_url}/open/envs?searchValue=JD_COOKIE&t=${Date.now()}`, {
         headers: {
             Accept: 'application/json',
@@ -16,9 +16,9 @@ const getCookie = async () => {
 const getToken = async () => {
     let body;
     try {
-        let client_id = config.client_id || process.env.client_id;
-        let client_secret = config.client_secret || process.env.client_secret;
-        let client_url = config.client_url || process.env.client_url;
+        let client_id = process.env.client_id;
+        let client_secret = process.env.client_secret;
+        let client_url = process.env.client_url;
         console.log(`${client_url}/open/auth/token?client_id=${client_id}&client_secret=${client_secret}`);
         body = await axios.get(`${client_url}/open/auth/token?client_id=${client_id}&client_secret=${client_secret}`);
         console.log(body);
